9c03662be6cb7243ebe7d79dc98300316815ab07
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-06-21 15:53:52 -0300

    exemplos INSERT

---
+++ Exemplos-INSERT/Ecemplo INSERT 03.sql
@@ -0,0 +1,26 @@
+﻿
+DROP TABLE pizzas; -- apaga a tabela pizzas
+
+CREATE TABLE  pizzas(
+	cliente		VARCHAR (100),
+	preco		DECIMAL(5,2),
+	diametro	SMALLINT,
+	fatias		TINYINT,
+	sabor_1		VARCHAR(50),
+	sabor_2		VARCHAR(50),
+	sabor_3		VARCHAR(50),
+	sabor_4		VARCHAR(50),	
+	com_borda	BIT,
+	com_brinde	BIT
+); 
+
+INSERT INTO pizzas VALUES('FrancisGod', 12.4, 70, 40, 'Calabresa', 'Franco com catupiry', 'Seis queijos', 'Algo e óleo', 1, 1);
+INSERT INTO pizzas
+(cliente, diametro, fatias, sabor_1, sabor_4, com_borda, com_brinde)
+ VALUES('Ygor', 45, 20, 'Pepino com tomte', 'Brócolis', 0, 0);
+ INSERT INTO pizzas 
+(cliente, preco, diametro, fatias, sabor_1, sabor_2, com_borda, com_brinde)
+VALUES('Stolfi', 40, 20, 16, 'Frango comm cebola', 'Frango sem cebola', 0, 1);
+INSERT INTo pizzas 
+(cliente, preco, diametro, fatias, sabor_1, sabor_2, sabor_3, com_borda, com_brinde)
+VALUES('Fadinha', 65, 35, 25, 'Italiana', 'Alemã', 'Britânia', 1, 0);
---
+++ Exemplos-INSERT/Exemplo INSERT 01.sql
@@ -0,0 +1,20 @@
+﻿/*
+	Criar uma tabela chamada pessoas,
+	com duas colunas nome e sobrenome
+*/
+
+CREATE TABLE pessoas(
+	nome VARCHAR (100),/* coluna que pode armazenar até 100 caracters */
+	sobrenome VARCHAR (170) /* coluna que pode armazenar até 170 caracteres */
+);
+
+/*
+	Inserir um registro na tabela pessoas
+	nas colunas nome e sobrenome
+*/
+TRUNCATE TABLE pessoas; -- apaga todos os registros da tabela pessoas 
+INSERT INTO pessoas VALUES ('João', 'da Silva');
+INSERT INTO pessoas VALUES ('Pedro', 'Machado');
+INSERT INTO pessoas VALUES ('Elriclédison', 'Pereira');
+INSERT INTO pessoas VALUES ('Jailson','da Silva');
+INSERT INTO pessoas VALUES ('Yudi', 'da Priscila');
